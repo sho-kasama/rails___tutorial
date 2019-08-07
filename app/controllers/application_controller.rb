2a88968b731322177a3f94eeff2dd104cf185228
sho-kasama <sho-kasama@trunk.fm>
2019-08-07 14:22:14 +0900

    add: hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception  # このコードがあると、Railsで生成される全てのフォームとAjaxセキュリティトークンが自動的に含まれる
+
+
+    def hello
+      render html: 'Hello world'  # render html でhtmlを返すようにしている
+    end
+
 end
